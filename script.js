e94a2f2f518f4581be1698eee6b2660e06b47c21
Sahil Raut <raut.sahil003@gmail.com>
2024-08-14 16:07:00 +0530

    Commit

--- script.js
+++ script.js
@@ -5,28 +5,52 @@ function toggleMenu(){
     icon.classList.toggle("open")
 }
 
+//for form msg
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxky3QvyhgY6tkbTlo7bhRj_wG-wMMSt98navubTWA4O2DHYg8ntt_sakxtmfuKJvmp/exec';
 
-function openVideoPopup(videoSrc) {
-    // Get the video modal
-    var videoModal = document.getElementById("videoModal");
+const form = document.forms['contact-form'];
+const messageDiv = document.getElementById('form-message');
 
-    // Set the video source dynamically
-    var videoIframe = document.createElement("iframe");
-    videoIframe.width = "600";
-    videoIframe.height = "375";
-    videoIframe.src = videoSrc;
-    videoIframe.frameBorder = "0";
-    videoIframe.allowFullscreen = true;
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  
+  messageDiv.textContent = '';
 
-    // Append the iframe to the video container in the modal
-    document.getElementById("videoContainer").innerHTML = "";
-    document.getElementById("videoContainer").appendChild(videoIframe);
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      messageDiv.textContent = "		Thank you! We will get back to you soon.";
+      messageDiv.style.color = 'green';
+    })
+    .catch(error => {
+      messageDiv.textContent = 'Error! ' + error.message;
+      messageDiv.style.color = 'red';
+    });
+});
 
-    // Display the modal
-    videoModal.style.display = "flex";
-}
 
-function closeVideoPopup() {
-    // Hide the modal
-    document.getElementById("videoModal").style.display = "none";
-}
+//
+//
+//function openVideoPopup(videoSrc) {
+//    // Get the video modal
+//    var videoModal = document.getElementById("videoModal");
+//
+//    // Set the video source dynamically
+//    var videoIframe = document.createElement("iframe");
+//    videoIframe.width = "600";
+//    videoIframe.height = "375";
+//    videoIframe.src = videoSrc;
+//    videoIframe.frameBorder = "0";
+//    videoIframe.allowFullscreen = true;
+//
+//    // Append the iframe to the video container in the modal
+//    document.getElementById("videoContainer").innerHTML = "";
+//    document.getElementById("videoContainer").appendChild(videoIframe);
+//
+//    // Display the modal
+//    videoModal.style.display = "flex";
+//}
+//
+//function closeVideoPopup() {
+//    // Hide the modal
+//    document.getElementById("videoModal").style.display = "none";
+//}
